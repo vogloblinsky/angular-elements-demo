17b225471a03d5c490dd4a6be6edfec1d4cc0e56
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-11-23 01:12:34 +0100

    Cleaning

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
-import {
-  enableProdMode,
-  NgModuleFactory,
-  CompilerFactory,
-  Type
-} from '@angular/core';
-
 import 'zone.js/dist/zone';
 
-import { platformBrowser } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { registerAsCustomElements } from '@angular/elements';
 
 import { HelloComponent, HelloModule } from './hello.component';
-
-interface WithName {
-  name: string;
-}
 
 registerAsCustomElements([HelloComponent], () =>
   platformBrowserDynamic().bootstrapModule(HelloModule)
